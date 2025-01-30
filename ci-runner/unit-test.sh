cdd2ecc4844d3a86d12af9cb9318b60e788b8f30
Shashwat Kandadai <shashwatak@gmail.com>
2025-01-30 01:20:33 -0800

    remove unavailable arg

--- ci-runner/unit-test.sh
+++ ci-runner/unit-test.sh
@@ -5,7 +5,6 @@ set -ex
 bazel test \
   --noshow_progress \
   --config=container \
-  --config=remote_cache \
   --cache_test_results=no \
   --test_env=BAZEL_BIN=$(bazel info bazel-bin) \
   --test_output=all \
