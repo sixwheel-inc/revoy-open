c5dee41f588f729bc0b2af56de252618728e33fa
Shashwat Kandadai <shashwatak@gmail.com>
2025-01-30 01:02:32 -0800

    try mounting bazel cache

--- .github/workflows/scripts/podman-check-formatting.sh
+++ .github/workflows/scripts/podman-check-formatting.sh
@@ -2,8 +2,10 @@
 
 image_url=$1
 
+mkdir -p ~/.cache/bazel
 podman run \
   --volume $GITHUB_WORKSPACE:/revoy-open \
+  --volume ~/.cache/bazel:/root/.cache/bazel:z \
   --workdir /revoy-open \
   $image_url \
   ./ci-runner/check-formatting.sh
