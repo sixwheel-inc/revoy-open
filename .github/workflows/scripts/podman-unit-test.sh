d966874bf68a0fa71355b6dc556cb7f854836987
Shashwat Kandadai <shashwatak@gmail.com>
2025-01-31 18:24:20 +0000

    fix disk cache path

--- .github/workflows/scripts/podman-unit-test.sh
+++ .github/workflows/scripts/podman-unit-test.sh
@@ -3,9 +3,14 @@
 image_url=$1
 
 mkdir -p ~/.cache/bazel
+mkdir -p ~/.cache/bazel-disk
+mkdir -p ~/.cache/bazel-repo
+
 podman run \
   --volume $GITHUB_WORKSPACE:/revoy-open \
   --volume ~/.cache/bazel:/root/.cache/bazel:z \
+  --volume ~/.cache/bazel-disk:/root/.cache/bazel-disk:z \
+  --volume ~/.cache/bazel-repo:/root/.cache/bazel-repo:z \
   --workdir /revoy-open \
   $image_url \
   ./ci-runner/unit-test.sh
