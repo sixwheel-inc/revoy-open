ebf7c225d643f52eb5ebb8e17d56e348582e1d38
Shashwat Kandadai <662734+shashwatak@users.noreply.github.com>
2025-02-27 17:11:58 -0800

    put collision unittests in collisions folder (#9)

--- planning/simpl/test-simpl.cpp
+++ planning/simpl/test-simpl.cpp
@@ -6,7 +6,6 @@
 #include "planning/footprint-overlap.h"
 #include "planning/make-scenario.h"
 #include "planning/mock-revoy-ev.h"
-#include "planning/occupancy-grid.h"
 #include "planning/simpl-mcap.h"
 #include "planning/simpl.h"
 #include "planning/types.h"
@@ -14,88 +13,6 @@
 #include "planning/add-footprint-to-grid.h"
 
 using namespace planning;
-
-TEST_CASE("test occupancy") {
-
-  constexpr uint8_t N = 255;
-  constexpr float D = 0.1;
-  constexpr float O = ((float)N) / 2.0 * D;
-
-  OccupancyGrid grid(N, N, D, D, O, O);
-
-  {
-    // not overlapping
-    Footprint a{{-10, -10}, {-5, -5}, {-10, -5}};
-    Footprint b{{10, 10}, {5, 5}, {10, 5}};
-    AddFootprintToGrid(a, grid);
-    CHECK(!grid.isFootprintOccupied(b));
-    grid.reset();
-  }
-  {
-    // overlapping
-    Footprint a{{9, 9}, {4, 4}, {9, 4}};
-    Footprint b{{10, 10}, {5, 5}, {10, 5}};
-    AddFootprintToGrid(a, grid);
-    CHECK(grid.isFootprintOccupied(b));
-    grid.reset();
-  }
-  {
-    // one inside the other
-    Footprint a{{10, 10}, {5, 5}, {10, 5}};
-    Footprint b{{8, 8}, {7, 7}, {8, 7}};
-    AddFootprintToGrid(a, grid);
-    CHECK(grid.isFootprintOccupied(b));
-    grid.reset();
-  }
-  {
-    // test that reset worked
-    Footprint b{{10, 10}, {5, 5}, {10, 5}};
-    CHECK(!grid.isFootprintOccupied(b));
-    grid.reset();
-  }
-}
-
-TEST_CASE("test collision detection") {
-  {
-    // not overlapping
-    Footprint a{{-10, -10}, {-5, -5}, {-10, -5}};
-    Footprint b{{10, 10}, {5, 5}, {10, 5}};
-    CHECK(!AreFootprintsOverlapping(a, b));
-  }
-  {
-    // overlapping
-    Footprint a{{9, 9}, {4, 4}, {9, 4}};
-    Footprint b{{10, 10}, {5, 5}, {10, 5}};
-    CHECK(AreFootprintsOverlapping(a, b));
-  }
-  {
-    // one inside the other
-    Footprint a{{8, 8}, {7, 7}, {8, 7}};
-    Footprint b{{10, 10}, {5, 5}, {10, 5}};
-    CHECK(AreFootprintsOverlapping(a, b));
-  }
-}
-
-TEST_CASE("test point in footprint") {
-  {
-    // not inside
-    Point a{-10, -10};
-    Footprint b{{10, 10}, {5, 5}, {10, 5}};
-    CHECK(!IsPointInFootprint(a, b));
-  }
-  {
-    // inside
-    Point a{9, 6};
-    Footprint b{{10, 10}, {5, 5}, {10, 5}};
-    CHECK(IsPointInFootprint(a, b));
-  }
-  {
-    // on
-    Point a{10, 7};
-    Footprint b{{10, 10}, {5, 5}, {10, 5}};
-    CHECK(IsPointInFootprint(a, b));
-  }
-}
 
 TEST_CASE("test obstacle scenario parameterized direction and distance") {
 
